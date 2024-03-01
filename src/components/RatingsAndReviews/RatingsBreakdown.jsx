17b7849da925dad7c79e9f0550ef90dc04c72af2
vlieu425 <121201991+vlieu425@users.noreply.github.com>
2024-02-29 16:25:51 -0800

    implementation to find totalReviews and averageRating

--- src/components/RatingsAndReviews/RatingsBreakdown.jsx
+++ src/components/RatingsAndReviews/RatingsBreakdown.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 
 
-const RatingsBreakdown =  () => {
+const RatingsBreakdown =  (props) => {
+
+  console.log("METADATA: ", props.reviewsMeta.ratings)
+
+  //use Object.values to create an array of values. Call .reduce on array with accum set to 0.
+  let totalReviews
+  if (props.reviewsMeta.ratings){
+    totalReviews = Object.values(props.reviewsMeta.ratings).reduce((acc, eachRating)=>{return(acc + Number(eachRating))}, 0);
+  }
+  console.log("totalReviews: ", totalReviews)
+
+  //sum all key*value pair divided by total reviews
+  let sum = 0
+  if (props.reviewsMeta.ratings){
+    for (var key in props.reviewsMeta.ratings) {
+      sum += key*props.reviewsMeta.ratings[key]
+    }
+  }
+  let averageRating = (sum/totalReviews).toFixed(1)
+  console.log("averageRating: ", averageRating)
+
 
 
   return (
     <div>
-      RatingBreakdown
+      {!isNaN(averageRating) ? <div>{averageRating}</div> : null}
       <RatingBar/>
       <CharacteristicBar/>
     </div>
