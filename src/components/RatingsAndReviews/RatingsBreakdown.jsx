2a46c36810b69590f5d0d13e1df91c0354828bd1
vlieu425 <121201991+vlieu425@users.noreply.github.com>
2024-03-01 09:50:40 -0800

    build out basic display for ratingBar

--- src/components/RatingsAndReviews/RatingsBreakdown.jsx
+++ src/components/RatingsAndReviews/RatingsBreakdown.jsx
@@ -3,31 +3,67 @@ import React from 'react';
 
 const RatingsBreakdown =  (props) => {
 
-  console.log("METADATA: ", props.reviewsMeta.ratings)
+  console.log("METADATA: ", props.reviewsMeta)
+  let metaData = props.reviewsMeta
 
   //use Object.values to create an array of values. Call .reduce on array with accum set to 0.
   let totalReviews
-  if (props.reviewsMeta.ratings){
-    totalReviews = Object.values(props.reviewsMeta.ratings).reduce((acc, eachRating)=>{return(acc + Number(eachRating))}, 0);
-  }
-  console.log("totalReviews: ", totalReviews)
-
-  //sum all key*value pair divided by total reviews
+  let averageRating
   let sum = 0
-  if (props.reviewsMeta.ratings){
-    for (var key in props.reviewsMeta.ratings) {
-      sum += key*props.reviewsMeta.ratings[key]
+  let fiveStarCount
+  let fourStarCount
+  let threeStarCount
+  let twoStarCount
+  let oneStarCount
+  let percentRecommend
+
+
+
+  if (metaData.ratings){
+    //
+    totalReviews = Object.values(metaData.ratings).reduce((acc, eachRating)=>{return(acc + Number(eachRating))}, 0);
+
+    for (var key in metaData.ratings) {
+      sum += key*metaData.ratings[key]
+      averageRating = (sum/totalReviews).toFixed(1)
     }
+
+    fiveStarCount = metaData.ratings['5']
+    fourStarCount = metaData.ratings['4']
+    threeStarCount = metaData.ratings['3']
+    twoStarCount = metaData.ratings['2']
+    oneStarCount = metaData.ratings['1']
+
+    percentRecommend = ((metaData.recommended.true/totalReviews).toFixed(2))*100
   }
-  let averageRating = (sum/totalReviews).toFixed(1)
+
+
+  console.log("totalReviews: ", totalReviews)
   console.log("averageRating: ", averageRating)
+  console.log("fiveStarCount: ", fiveStarCount)
+  console.log("fourStarCount: ", fourStarCount)
+  console.log("threeStarCount: ", threeStarCount)
+  console.log("twoStarCount: ", twoStarCount)
+  console.log("oneStarCount: ", oneStarCount)
+  console.log("percentRecommend: ", percentRecommend)
 
 
 
   return (
-    <div>
-      {!isNaN(averageRating) ? <div>{averageRating}</div> : null}
-      <RatingBar/>
+    <div style={{border: "2px solid black", width: "30vw"}}>
+      {!isNaN(averageRating) ?
+        <div>{averageRating}</div>
+        :
+        null
+      }
+      <div>
+        <RatingBar/>
+        <RatingBar/>
+        <RatingBar/>
+        <RatingBar/>
+        <RatingBar/>
+      </div>
+      {`${percentRecommend}% of people recommend`}
       <CharacteristicBar/>
     </div>
   )
@@ -35,12 +71,44 @@ const RatingsBreakdown =  (props) => {
 
 const RatingBar =  () => {
 
-
   return (
-    <div>
-      RatingBar
-    </div>
+    <div
+      style={{
+        border: "2px solid red",
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column'
+      }}
+    >
 
+      <div>RatingBar </div>
+
+      <div
+        style={{
+          width: '90%',
+          display: 'flex',
+          alignItems: 'center',
+          height: '5px',
+          backgroundColor: '#D3D3D3',
+          borderRadius: '8px',
+          margin: '5px'
+        }}
+      >
+
+        <div
+          style={{
+            width: '25%',
+            height: '100%',
+            backgroundColor: '#009900'
+          }}
+        >
+        </div>
+
+      </div>
+
+
+
+    </div>
   )
 }
 
