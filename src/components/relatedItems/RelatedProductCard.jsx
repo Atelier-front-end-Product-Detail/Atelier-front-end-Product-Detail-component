93e42ebfddec45fc9ace2a957bcffc5311c0478d
Michael O'Brien <mob61887@gmail.com>
2024-03-04 18:29:26 -0500

    removed .jsx from App and RelatedProducts imports, cleaned up bridge, added dotenv variable

--- src/components/relatedItems/RelatedProductCard.jsx
+++ src/components/relatedItems/RelatedProductCard.jsx
@@ -9,7 +9,7 @@ function RelatedProductCard({ productId, bridge, setProductId }) {
   const [productPhotoIndex, setProductPhotoIndex] = useState(0);
   const [productReviews, setProductReviews] = useState(0);
 
-  const imageNotFound = 'https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg';
+  const imageNotFound = process.env.IMAGE_NOT_FOUND;
 
   // HELPER FUNCTIONS
   const incrementPhotoIndex = (e) => {
