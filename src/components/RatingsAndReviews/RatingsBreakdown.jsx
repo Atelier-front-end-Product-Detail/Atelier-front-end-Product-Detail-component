94de1a63cf4d558ef6b97232f139b1b05ea7967e
vlieu425 <121201991+vlieu425@users.noreply.github.com>
2024-03-04 23:48:20 -0800

    test file for RatingsBreakdown component

--- src/components/RatingsAndReviews/RatingsBreakdown.jsx
+++ src/components/RatingsAndReviews/RatingsBreakdown.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './RatingsBreakdown.css';
 
-
 function RatingsBreakdown({ reviewsMeta }) {
   // console.log('METADATA: ', reviewsMeta);
 
