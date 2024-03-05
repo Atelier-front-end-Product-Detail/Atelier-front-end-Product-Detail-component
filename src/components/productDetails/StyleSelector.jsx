ca7ae042cae467da545cc561abb24681b5cf7a2e
Eric Yap <ericy2296@gmail.com>
2024-03-04 16:10:11 -0800

    Fixed linter errors, implemented Size, quantity, add to cart

--- src/components/productDetails/StyleSelector.jsx
+++ src/components/productDetails/StyleSelector.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-
-const StyleSelector = ({ styles }) => {
+function StyleSelector({ styles }) {
   const [selectedStyle, setSelectedStyle] = useState(styles[0]);
 
-  //handler
   const handleStyleClick = (style) => {
     if (style !== selectedStyle) {
       setSelectedStyle(style);
@@ -15,9 +14,15 @@ const StyleSelector = ({ styles }) => {
     <div className="style-selector">
       <div className="style-title">{selectedStyle.name}</div>
       <div className="styles-container">
-        {styles.map((style, index) => (
-          <div key={style.name} className={`style-thumbnail ${selectedStyle === style ? 'selected' : ''}`}
-               onClick={() => handleStyleClick(style)}>
+        {styles.map((style) => (
+          <div
+            key={style.name}
+            className={`style-thumbnail ${selectedStyle === style ? 'selected' : ''}`}
+            onClick={() => handleStyleClick(style)}
+            onKeyDown={(e) => e.key === 'Enter' && handleStyleClick(style)}
+            role="button"
+            tabIndex={0}
+          >
             <img src={style.photos[0].thumbnail_url} alt={style.name} />
             {selectedStyle === style && <div className="checkmark">✔</div>}
           </div>
@@ -25,6 +30,19 @@ const StyleSelector = ({ styles }) => {
       </div>
     </div>
   );
+}
+
+StyleSelector.propTypes = {
+  styles: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      photos: PropTypes.arrayOf(
+        PropTypes.shape({
+          thumbnail_url: PropTypes.string,
+        }),
+      ),
+    }),
+  ).isRequired,
 };
 
 export default StyleSelector;
