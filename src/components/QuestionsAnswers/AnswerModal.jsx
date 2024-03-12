d26761c39c288ca8eb6aa2718a1c81f7a6a79352
Nhu Le <nhu.le1236@gmail.com>
2024-03-12 15:32:28 -0400

    fixed eslint errors

--- src/components/QuestionsAnswers/AnswerModal.jsx
+++ src/components/QuestionsAnswers/AnswerModal.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-console */
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
--- src/components/QuestionsAnswers/QuestionsAnswersList.jsx
+++ src/components/QuestionsAnswers/QuestionsAnswersList.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import QuestionAnswerEntry from './QuestionAnswerEntry';
 
 function QuestionsAnswersList({
@@ -13,9 +14,9 @@ function QuestionsAnswersList({
   const questionsMap = (object) => {
     const questionsArr = [];
 
-    for (const key in object) {
+    Object.keys(object).forEach((key) => {
       questionsArr.push(object[key]);
-    }
+    });
 
     questionsArr.sort((a, b) => b.question_helpfulness - a.question_helpfulness);
     return questionsArr;
@@ -24,10 +25,35 @@ function QuestionsAnswersList({
   return (
     <div className="questions-answers-list">
       {questionsMap(questionsData).slice(0, dataNum).map((question) => (
-        <QuestionAnswerEntry productName={productName} key={question.question_id} bridge={bridge} question={question} handleQuestionHelpful={handleQuestionHelpful} handleQuestionReport={handleQuestionReport} />
+        <QuestionAnswerEntry
+          productName={productName}
+          key={question.question_id}
+          bridge={bridge}
+          question={question}
+          handleQuestionHelpful={handleQuestionHelpful}
+          handleQuestionReport={handleQuestionReport}
+        />
       ))}
     </div>
   );
 }
+
+QuestionsAnswersList.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape()),
+  bridge: PropTypes.objectOf(PropTypes.func),
+  dataNum: PropTypes.number,
+  handleQuestionHelpful: PropTypes.func,
+  handleQuestionReport: PropTypes.func,
+  productName: PropTypes.string,
+};
+
+QuestionsAnswersList.defaultProps = {
+  data: '',
+  bridge: '',
+  dataNum: '',
+  handleQuestionHelpful: '',
+  handleQuestionReport: '',
+  productName: '',
+};
 
 export default QuestionsAnswersList;
