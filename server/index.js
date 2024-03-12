33cd38eaf52484abf926ae3e5e5ec495e1e13724
Michael O'Brien <mob61887@gmail.com>
2024-03-12 17:52:22 -0400

    installed morgan

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
 const Path = require('path');
+const axios = require('axios');
 require('dotenv').config();
-const axios = require('axios')
+
+const app = express();
 
 app.use(express.json());
+
+app.use(morgan('dev'));
+
 app.use(express.static(Path.join(__dirname, '../dist')));
 
-
 app.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}`));
-
